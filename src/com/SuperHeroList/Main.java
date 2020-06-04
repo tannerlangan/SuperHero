1c3ddce7794b4a2c82fa564584bedae0be11f4e5
tannerlangan <t.langan.07@gmail.com>
2020-06-04 13:34:19 -0700

    Added functionality to SuperHero.java to remove superheroes, created getter for heros names, created constructors to hold superheroes. Added method to create arraylist of Superheroes based on Superhero.java constructor, added list all superheroes option in main function

--- src/com/SuperHeroList/Main.java
+++ src/com/SuperHeroList/Main.java
@@ -1,6 +1,5 @@
 package com.SuperHeroList;
 import java.util.*;
-import java.io.*;
 import java.lang.*;
 
 
@@ -10,6 +9,15 @@ import java.lang.*;
 
 
 public class Main {
+
+    public static ArrayList<SuperHero> superheroList = new ArrayList<>();
+
+
+    public static void addSuperHero(SuperHero newHero){
+
+        superheroList.add(newHero);
+
+    }
 
 
     public static void main(String arg[]) {
@@ -24,7 +32,13 @@ public class Main {
                 choice = in.nextInt();
             } while (choice < 0 || choice > 6);
 
-            if (choice == 2) {
+            if(choice == 1){
+              for (int i = 0; i < superheroList.size(); i++){
+                  System.out.println(superheroList.get(i).toString());
+              }
+            }
+
+            else if (choice == 2) {
                 System.out.println("Please Enter Superheroes name");
                 String name = in.next();
                 System.out.println("Please enter their superpower");
@@ -33,7 +47,15 @@ public class Main {
                 double height = in.nextDouble();
                 System.out.println("Please enter how many civilians they have saved");
                 int civSaved = in.nextInt();
-                superhero.newHero(name, power, height, civSaved);
+                SuperHero newHero = new SuperHero(name, power, height, civSaved);
+                addSuperHero(newHero);
+            }
+            else if(choice == 3){
+                System.out.println("Please Select Superhero to be removed based on their ID");
+                int removeChoice = in.nextInt();
+                SuperHero currentHero = superheroList.get(removeChoice);
+                String heroName = currentHero.getName();
+                superhero.removeHero(superheroList, removeChoice, heroName);
             }
         }while(choice != 0);
 
--- src/com/SuperHeroList/SuperHero.java
+++ src/com/SuperHeroList/SuperHero.java
@@ -1,16 +1,46 @@
 package com.SuperHeroList;
 
+import java.util.ArrayList;
+
 public class SuperHero{
 
+    private String name, superpower;
+    private  double height;
+    private int civiliansSaved;
 
-    public void newHero(String name, String superpower, double heightInCM, int civiliansSaved){
+    public  SuperHero(String name, String superpower, double heightInCM, int civiliansSaved){
+        this.name = name;
+        this.superpower = superpower;
+        this.height = heightInCM;
+        this.civiliansSaved = civiliansSaved;
+    }
+
+    public  SuperHero(){
+    }
+
+    public String getName(){
+        return name;
+    }
+
+    public String toString(){
+        return "Name: " + name + " Superpower: " + superpower + " Height: " + height + " Civilians Saved: " + civiliansSaved;
+    }
+
+    public void removeHero(ArrayList<SuperHero> heroList, int indexToBeRemoved, String superHero){
+
+        heroList.remove(indexToBeRemoved);
+        System.out.println(superHero + " has been removed from the Superhero List");
+    }
+
+    public void listHeroes(ArrayList<String> heroList){
+
+        for(int i = 0; i < heroList.size(); i++){
+            System.out.print("ID: " + i);
+            System.out.println(heroList.get(i));
+
+        }
 
     }
 
-    public static void main(String arg[]){
-
-
-
-    }
 
 }
